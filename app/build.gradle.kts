8f363f5ba4aa52be7a58ac90925ef6b98a95bf84
Lancimator <lancimator@gmail.com>
2025-08-03 09:44:25 +0200

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.lancimator.stopaddiction"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1007
+        versionCode = 1010
         versionName = "1.0"
     }
 
